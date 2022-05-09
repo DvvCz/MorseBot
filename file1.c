a452e56c1d84b3b1d59ae43da07c0f6914716a01
David C <86097860+DvvCz@users.noreply.github.com>
2022-05-08 17:13:08 -0700

    Update file1.c

--- file1.c
+++ file1.c
@@ -1,21 +1,25 @@
 // File file1.c
 // file3.c but using Pixy as a backend, instead of color sensor
-#include "file0.c"
+#include "file4.c"
 
-int R, G, B;
+uint8_t R, G, B;
 
-inline void loadColors() {
+#define LOADCOL_OK PIXY_RESULT_OK
+#define LOADCOL_ERROR PIXY_RESULT_ERROR
+#define LOADCOL_BUSY PIXY_RESULT_BUSY
+
+inline int loadColors() {
 	vGetRGB( ROBOT_WIDTH / 2, ROBOT_HEIGHT / 2, &R, &G, &B, true );
 }
 
-inline int getRed() {
+inline uint8_t getRed() {
 	return R;
 }
 
-inline int getGreen() {
+inline uint8_t getGreen() {
 	return G;
 }
 
-inline int getBlue() {
+inline uint8_t getBlue() {
 	return B;
 }
