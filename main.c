e1eff86d004d65a790bd79bbd3ef2fb0aa19b81b
David C <86097860+DvvCz@users.noreply.github.com>
2022-05-10 11:14:59 -0700

    Make main configs

--- main.c
+++ main.c
@@ -1,10 +1,40 @@
+/*
+	Configs Start
+*/
+#define PIN_AUDIO 27
+
+// These are flipped for some reason :/
+// rx is PIN_TX, tx is PIN_RX. If the robot randomly stops working try swapping these.
+#define PIN_RX 6
+#define PIN_TX 10
+
+// Try to sync these with the website
+#define CHAR_LEN 300
+#define LONG_LEN 900
+#define WORD_LEN 2700 // ms
+#define DOWNTIME_LEN 150
+
+// Might need to be different?
+#define PAUSE_TIME DOWNTIME_LEN
+
+// R, G, B
+#define COLOR_OFF() pSetLED(0, 0, 255); // blue (detected light off)
+#define COLOR_ON() pSetLED(255, 0, 255); // purple (detected light)
+#define COLOR_START() pSetLED(0, 255, 0); // green (started scanning for morse code)
+#define COLOR_FAIL() pSetLED(255, 255, 0); // yellow (failed to get color result from pixy camera)
+
+// Baud rate between the robot and the pixy camera through UART.
+// Apparently the max is 230 kbaud (230,000 ??) https://docs.pixycam.com/wiki/doku.php?id=wiki:v2:porting_guide
+#define PIXY_BAUD 19200
+
+/*
+	Configs End
+*/
+
 #include <assert.h>
 #include "simpletools.h"
 #include "text2speech.h"
 #include "stdbool.h"
-
-/* Configs */
-#define PIN_AUDIO 27
 
 int ROBOT_WIDTH, ROBOT_HEIGHT;
 
@@ -38,7 +68,7 @@ int elapsed(int cnt) {
 }
 
 int main() {
-	channel = fdserial_open(PIN_RX, PIN_TX, 0, 19200);
+	channel = fdserial_open(PIN_RX, PIN_TX, 0, PIXY_BAUD);
 	assert(channel != NULL && "Failed to create channel");
 
 	// https://docs.pixycam.com/wiki/doku.php?id=wiki:v2:porting_guide
@@ -51,11 +81,6 @@ int main() {
 
 	pGetResolution(&ROBOT_WIDTH, &ROBOT_HEIGHT);
 	printlnf("Resolution: %dx%d", ROBOT_WIDTH, ROBOT_HEIGHT);
-	
-	pSetLED(0, 255, 0);
-
-	//initTTS();
-	//setVolume(100);
 
 	// How long the light has been either off or on.
 	bool is_light = false;
@@ -64,15 +89,13 @@ int main() {
 	char buf[1000] = "";
 	uint16_t pos = 0;
 
-	#define CHAR_LEN 100
-	#define LONG_LEN CHAR_LEN * 2
-	#define WORD_LEN LONG_LEN * 2 // ms
-
 	int before = CNT;
 	int last_status = CNT;
+
+	COLOR_START();
 	while ( elapsed(before) < 15000 ) {
 		if ( loadColors() != LOADCOL_OK ) {
-			pSetLED(0, 255, 0);
+			COLOR_FAIL();
 
 			pause(200);
 			continue;
@@ -88,11 +111,11 @@ int main() {
 			// Light ON
 			if (!is_light) {
 				if (status_for > WORD_LEN) {
-					buf[pos] = '/';
-					pos++;
+					buf[pos++] = ' ';
+					buf[pos++] = '/';
+					buf[pos++] = ' ';
 				} else if (status_for > CHAR_LEN) {
-					buf[pos] = ' ';
-					pos++;
+					buf[pos++] = ' ';
 				} else {
 					// Skip.
 					// buf[pos] = '';
@@ -103,24 +126,22 @@ int main() {
 				is_light = true;
 				status_for = 0;
 
-				pSetLED(255, 0, 255);
+				COLOR_ON();
 			}
 		} else if (is_light) {
 			// Light OFF (Was ON last time)
 			if (status_for > LONG_LEN) {
-				buf[pos] = '-';
-				pos++;
+				buf[pos++] = '-';
 			} else {
-				buf[pos] = '.';
-				pos++;
+				buf[pos++] = '.';
 			}
 
 			last_status = CNT;
-			pSetLED(0, 0, 255);
+			COLOR_OFF();
 			is_light = false;
 		}
 
-		pause(100);
+		pause(PAUSE_TIME);
 	}
 	buf[pos] = '\0';
 
