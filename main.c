447d6d4da781c7780e7a2fed56a6f2f84258fc16
David C <86097860+DvvCz@users.noreply.github.com>
2022-05-26 00:07:16 -0700

    before i forget

--- main.c
+++ main.c
@@ -13,20 +13,13 @@
 #define CHAR_LEN 100
 #define LONG_LEN 800
 #define WORD_LEN 2700 // ms
-#define DOWNTIME_LEN 100
+#define DOWNTIME_LEN 50
 
 #define SCAN_TIME 10000 // max time to scan for morse code for in milliseconds (40000 = 40 seconds)
 #define END_LEN WORD_LEN * 2
 
 // Might need to be different?
-#define PAUSE_TIME DOWNTIME_LEN
-
-// R, G, B (255 - 0)
-#define COLOR_OFF() pSetLED(0, 0, 255); // blue (detected light off)
-#define COLOR_ON() pSetLED(255, 0, 255); // purple (detected light)
-#define COLOR_START() pSetLED(0, 255, 0); // green (started scanning for morse code)
-#define COLOR_FAIL() pSetLED(255, 255, 0); // yellow (failed to get color result from pixy camera)
-#define COLOR_WAIT() pSetLED(0, 0, 0); // no color (waiting for signal to start scanning)
+#define PAUSE_TIME 50
 
 // Baud rate between the robot and the pixy camera through UART. (Default 19200)
 // Apparently the max is 230 kbaud (230,000 ??) https://docs.pixycam.com/wiki/doku.php?id=wiki:v2:porting_guide
@@ -47,25 +40,8 @@ int ROBOT_WIDTH, ROBOT_HEIGHT;
 #include "file4.c"
 #include "file5.c"
 
-talk *TTS;
-
-void initTTS() {
-	// L, R pins.
-	// Left as -1 for mono audio to pin 27.
-	TTS = talk_run(PIN_AUDIO_L, PIN_AUDIO_R);
-	talk_set_speaker(TTS, 1, 100);
-}
-
-inline void setVolume(int vol) {
-	talk_setVolume(TTS, vol);
-}
-
-inline void spell(char* txt) {
-	talk_spell(TTS, txt);
-}
-
 inline void say(char* txt) {
-	talk_say(TTS, txt);
+	print("<tts>%s</tts>", txt);
 }
 
 // Returns time elapsed since certain timestamp in ms.
@@ -94,45 +70,60 @@ int elapsed(int cnt) {
 -on 8 403
 -off 9 80 0
 Buf: [...---...!]
+
+vs
+-off 0 10 0
+-on 0 141
+-off 1 211 0
+-on 1 70
+-off 2 70 0
+-on 2 706
+-off 3 141 0
+-on 3 141
+-off 4 70 0
+-on 4 1977
+-off 5 70 0
+-on 5 1271
+-off 6 70 0
+-on 6 494
+-off 7 70 0
+-on 7 353
+-off 8 70 0
+-on 8 423
+....--..Got word ....--...!
+
 */
 
 int main() {
 	channel = fdserial_open(PIN_RX, PIN_TX, 0, PIXY_BAUD);
 	assert(channel != NULL && "Failed to create channel");
 
-	// https://docs.pixycam.com/wiki/doku.php?id=wiki:v2:porting_guide
-	// Pixy2 Demo Code
-	println("Pixy2 Camera Demo");
-
-	pGetResolution(&ROBOT_WIDTH, &ROBOT_HEIGHT);
-	printlnf("Resolution: %dx%d", ROBOT_WIDTH, ROBOT_HEIGHT);
-
-
 	uint8_t r, g, b;
-	COLOR_WAIT();
 
 	// Wait until we see green. That is the signal to start scanning for morse code. (and to stop)
-	do {
+	while (sanitycheck(2) && (r > 60 || g < 230 || b > 60)) {
 		if ( vGetRGB( ROBOT_WIDTH / 2, ROBOT_HEIGHT / 2, &r, &g, &b, true ) != PIXY_RESULT_OK ) break;
 		pause(PAUSE_TIME);
-	} while( r > 60 || g < 230 || b > 60 );
-
-	COLOR_START();
-
-	pause(1000); // Wait for initial green to change]
-
-	COLOR_FAIL();
+	}
 
 	int before = CNT;
 	int light_on = false;
 	int last = CNT; // Time current status was achieved.
 	int status_for = 0;
 
-	char buf[1000] = "";
+	char buf[1000];
+	int pos;
 
-	int pos = 0;
+	ml:
+	buf[1000] = "";
+	pos = 0;
 
-	while ( elapsed(before) < SCAN_TIME ) {
+	print("<tts>Started</tts>\n");
+
+	while ( true ) {
+		getStr(buf, 128); // Load buffer into memory
+		if (!sanitycheck()) break;
+
 		if ( vGetRGB( ROBOT_WIDTH / 2, ROBOT_HEIGHT / 2, &r, &g, &b, true ) != PIXY_RESULT_OK ) break;
 
 		if ( r >= 180 && g >= 180 && b >= 180 ) {
@@ -146,8 +137,6 @@ int main() {
 					pos++;
 				}
 
-				print("-off %d %d %b", pos, elapsed(last), elapsed(last) >= LONG_LEN);
-
 				last = CNT;
 				light_on = true;
 			}
@@ -157,7 +146,6 @@ int main() {
 			break;
 		} else { // if ( r <= 90 && g <= 90 && b <= 90 ) {
 			if (light_on) {
-				print("-on %d %d", pos, elapsed(last));
 				if (elapsed(last) >= LONG_LEN) {
 					buf[pos] = '-';
 				} else {
@@ -172,23 +160,20 @@ int main() {
 
 		pause(PAUSE_TIME);
 	}
-	buf[pos] = '\0';
 
-	COLOR_START();
-
-	printlnf("\nBuf: [%s]", buf);
+	fwrite(buf, 1, pos, stdout);
 
 	char out[] = "";
 	if ( translateMorse(buf, out) == MORSE_OK ) {
-		print("Success: [%s]\n", out);
+		// Morse code was translated successfully.
+		print("Success: [%s]<tts>%s</tts>\n", out, out);
 		pSetLED(0, 255, 0);
 
-		// Morse code was translated successfully.
-		// Call "spell" tts on the output message.
-		// Can also try "say"
-		spell( out );
+		goto ml;
 	} else {
-		print("Failed!\n");
+		print("<tts>Failed!</tts>\n");
 		pSetLED(255, 0, 0);
+		
+		goto ml;
 	}
 }
